1ccb9b0dd7d603142863fdf45e4f105521211d55
h0wter <91832203+h0wter@users.noreply.github.com>
2023-01-27 11:58:07 +0000

    Deploying to gh-pages from @ h0wter/goit-react-hw-08-phonebook@3ca94370b0dc46c85db7a13c2216cfc55a664921 🚀

---
+++ static/js/266.c91f4020.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{1266:function(e,n,o){o.r(n),o.d(n,{default:function(){return h}});var t=o(3329);function h(){return(0,t.jsx)("h1",{children:"home"})}}}]);
+//# sourceMappingURL=266.c91f4020.chunk.js.map
